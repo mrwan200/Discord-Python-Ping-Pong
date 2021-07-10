dc80d36643f0b94641f9598476df216702e1ddd6
M-307 <unturnedmc1@gmail.com>
2021-07-10 22:29:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="")
 
 @bot.event
-async def on_ready():
-    print("Bot ready.")
+async def on_ready(): # เช็คว่าบอทออนไลน์หรือยัง
+    print("Bot ready.") # ถ้าบอททำงาน คำสั่งนี้(print) จะทำงาน 
 
 @bot.event
-async def on_message(message):
-    if message.content == ">ping":
+async def on_message(message): # ใส่คำที่ให้ผู้ใช้งานป้อน
+    if message.content == ">ping": # ใส่คำที่บอทตอบกลับ
         await message.channel.send("Pong.")
+        # ถ้าUser พิมพ์ >ping บอทจะตอบกลับว่า pong
 
-bot.run("ODYzNDI0ODcxOTI0MjM2Mjg5.YOmtEw.ksKpU3epIew38MQRRjXKzGUH_LE")
+bot.run("YOUR BOT TOKEN HERE") # ใส่โทเคนของบัญชีที่จะให้บอทไปสิง
